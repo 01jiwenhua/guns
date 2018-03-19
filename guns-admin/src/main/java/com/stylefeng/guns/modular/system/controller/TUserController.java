8f063bdfbff6809241e2cd859c797b80fd21d7b2
wangzy <wangzy0228@163.com>
2018-03-20 00:40:56 +0800

    test push

--- guns-admin/src/main/java/com/stylefeng/guns/modular/system/controller/TUserController.java
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/system/controller/TUserController.java
@@ -83,9 +83,10 @@ public class TUserController extends BaseController {
     @RequestMapping(value = "/list")
     @ResponseBody
     public Object list(String condition) {
-        List<Map<String, Object>> userList = tUserService.getUserList();
-        System.out.println(userList.toString());
-        return userList;
+        return tUserService.selectList(null);
+//        List<Map<String, Object>> userList = tUserService.getUserList();
+//        System.out.println(userList.toString());
+//        return userList;
     }
 
     /**
